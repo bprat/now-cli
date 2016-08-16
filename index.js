57a6d002abee791e38a6b0d264af16841fdba9c4
Bharath Prathipati <royalbharath@gmail.com>
2016-08-15 21:27:13 -0700

    started pulling

--- index.js
+++ index.js
@@ -3,56 +3,66 @@
 
 var fileData,
     fs = require('fs'),
-    instance,
     program = require('commander'),
     request = require('request'),
     config = require('config'),
-    username, password;
+    username = config.get('creds.user'),
+    password = config.get('creds.passwd'),
+    instance = config.get('instance.name');
 
 program
-.version('0.0.1')
-.command('pull <file>')
-.description('Pull a script from ServiceNow')
-.option('-f', 'force pull')
-.action();
-
-program
-.version('0.0.1')
-.command('push <file>')
-.description('Push file to the SN instance')
-.option('-f', 'hard push')
-.action(function(file) {
-  username = config.get('creds.user');
-  password = config.get('creds.passwd');
-  instance = config.get('instance.name');
-  console.log('password: ' + password);
-  console.log('file: %s ', file);
-  fs.readFile(file, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    fileData = data;
-    console.log('fileData: %s', fileData);
-    var options = {
-      url: 'https://' + instance + '.service-now.com/api/now/table/sys_script_include/9d1b62d0db95ea00dd6af93baf9619b4',
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': "Basic " + new Buffer(username + ":" + password).toString("base64")
-      },
-      json: {'script': fileData}
-    }
-    // Start the request
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        // Print out the response body
-        //console.log(body)
-      } else {
-        console.log('error: ' + response.statusCode);
-      }
-    })
+  .version('0.0.1')
+  .command('pull <type> <file>')
+  .description('Pull a script from ServiceNow')
+  .option('-f', 'force pull')
+  .action(function (type, file) {
+    console.log('pulling %s of type %s', file, type);
+    console.log('table for %s is %s', type, config.table[type])
+    // var options = {
+    //   url: 'https://' + instance + '.service-now.com/api/now/table/sys_script_include/9d1b62d0db95ea00dd6af93baf9619b4',
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept': 'application/json',
+    //     'Authorization': "Basic " + new Buffer(username + ":" + password).toString("base64")
+    //   },
+    //   json: {'script': fileData}
+    // }
   });
-});
+
+program
+  .version('0.0.1')
+  .command('push <file>')
+  .description('Push file to the SN instance')
+  .option('-f', 'hard push')
+  .action(function(file) {
+    console.log('pushing %s ', file);
+    fs.readFile(file, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      fileData = data;
+      // console.log('fileData: %s', fileData);
+      var options = {
+        url: 'https://' + instance + '.service-now.com/api/now/table/sys_script_include/9d1b62d0db95ea00dd6af93baf9619b4',
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': "Basic " + new Buffer(username + ":" + password).toString("base64")
+        },
+        json: {'script': fileData}
+      }
+      // Start the request
+      request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          // Print out the response body
+          //console.log(body)
+        } else {
+          console.log('error: ' + response.statusCode);
+        }
+      })
+    });
+  });
 
 program.parse(process.argv);
